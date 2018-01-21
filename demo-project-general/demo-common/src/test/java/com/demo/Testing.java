aaa1612fd0c57875318c2300daf997e8b52ada86
xusong18 <scxs1818@163.com>
2018-01-21 17:15:01 +0800

    第一次测试提交代码

---
+++ demo-project-general/demo-common/src/test/java/com/demo/Testing.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+public class Testing {
+    /**
+     * idea 自动补齐方法快捷键 ctrl+j
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.err.println("asdasdasd");
+    }
+}
